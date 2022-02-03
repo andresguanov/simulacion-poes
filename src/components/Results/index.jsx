7835fafa58f554039b44b55f09de12dae0d43757
andresguanov <andresguanov@gmail.com>
2022-02-03 12:45:08 -0500

    Change decimals in Results component

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,8 +1,13 @@
 import './Results.css'
 
-export const Results = ({ volumes, cellVolume, sectorColors }) => {
+export const Results = ({
+    volumes,
+    cellVolume,
+    sectorColors
+}) => {
 
     const { bulkVolume, porousVolume, waterVolume, oilVolume } = volumes
+    const decimals = 2
 
     return (
 
@@ -15,27 +20,27 @@ export const Results = ({ volumes, cellVolume, sectorColors }) => {
                     <h2 className='title'>Resultados</h2>
 
 
-                    <p><strong>Volumen de la celda:</strong> {(7758 * cellVolume).toFixed(2)} bls</p>
+                    <p><strong>Volumen de la celda:</strong> {(7758 * cellVolume).toFixed(decimals)} bls</p>
 
                     <div className='Sector-results'>{bulkVolume.map((volume, i) => (
                         <div key={i} >
                             <h3 style={{ color: sectorColors[i] }}>Sector {i + 1}</h3>
 
-                            <p><strong>Volumen total:</strong> {volume.toFixed(2)} MM Bls</p>
-                            <p><strong>Volumen poroso:</strong> {porousVolume[i].toFixed(2)} MM Bls</p>
-                            <p><strong>Volumen de agua:</strong> {waterVolume[i].toFixed(2)} MM Bls</p>
-                            <p><strong>Volumen de petróleo:</strong> {oilVolume[i].toFixed(2)} MM Bls</p>
+                            <p><strong>Volumen total:</strong> {volume.toFixed(decimals)} MM Bls</p>
+                            <p><strong>Volumen poroso:</strong> {porousVolume[i].toFixed(decimals)} MM Bls</p>
+                            <p><strong>Volumen de agua:</strong> {waterVolume[i].toFixed(decimals)} MM Bls</p>
+                            <p><strong>Volumen de petróleo:</strong> {oilVolume[i].toFixed(decimals)} MM Bls</p>
                         </div>
                     ))}</div>
 
                     <h3>Global</h3>
-                    <p><strong>Volumen total:</strong> {bulkVolume.reduce((a, b) => a + b).toFixed(2)}MM Bls
+                    <p><strong>Volumen total:</strong> {bulkVolume.reduce((a, b) => a + b).toFixed(decimals)}MM Bls
                     </p>
-                    <p><strong>Volumen poroso total:</strong> {porousVolume.reduce((a, b) => a + b).toFixed(2)}MM Bls
+                    <p><strong>Volumen poroso total:</strong> {porousVolume.reduce((a, b) => a + b).toFixed(decimals)}MM Bls
                     </p>
-                    <p><strong>Volumen de agua total:</strong> {waterVolume.reduce((a, b) => a + b).toFixed(2)}MM Bls
+                    <p><strong>Volumen de agua total:</strong> {waterVolume.reduce((a, b) => a + b).toFixed(decimals)}MM Bls
                     </p>
-                    <p><strong>POES:</strong> {oilVolume.reduce((a, b) => a + b).toFixed(2)}MM Bls
+                    <p><strong>POES:</strong> {oilVolume.reduce((a, b) => a + b).toFixed(decimals)}MM Bls
                     </p>
 
                 </div>
